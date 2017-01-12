d6980c62027dddb4fd63a4a7e0bef5ee0b276eb9
Bubnov <bubnov.art@gmail.com>
2017-01-12 04:13:21 +0300

    updates

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -1,8 +1,9 @@
+#encoding: utf-8
 Spree::BaseHelper.class_eval do 
   def breadcrumbs(taxon, product)
       return "" if current_page?("/") || taxon.nil?
       separator = content_tag(:span, content_tag(:i, " ",class: ["fa", "fa-angle-right"]), class: "delimiter")
-      crumbs = [link_to(content_tag(:span, Spree.t(:home), itemprop: "name"), spree.root_path, itemprop: "url") + separator]
+      crumbs = [link_to(content_tag(:span, "Главная", itemprop: "name"), spree.root_path, itemprop: "url") + separator]
 
       if taxon
          # crumbs << taxon.ancestors.collect { |ancestor| link_to(content_tag(:span, ancestor.name, itemprop: "name"), seo_url(ancestor), itemprop: "url") + separator }
