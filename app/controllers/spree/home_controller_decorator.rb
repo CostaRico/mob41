1eeeead4b00ad59373c88753f9a41d2ceef72bcf
Bubnov <bubnov.art@gmail.com>
2017-02-01 08:08:02 +0300

    upd view

--- app/controllers/spree/home_controller_decorator.rb
+++ app/controllers/spree/home_controller_decorator.rb
@@ -4,6 +4,6 @@ Spree::HomeController.class_eval do
 	    @products = @searcher.retrieve_products
 	    @products = @products.includes(:possible_promotions) if @products.respond_to?(:includes)
 	    @taxonomies = Spree::Taxonomy.includes(root: :children)
-		@feat_products = Spree::Product.first(12)
+		@feat_products = Spree::Product.offset(rand(Spree::Product.count)).limit(6)
 	end
 end
