1004398e9ccb4c72db013bc6cd52142e4c1d9186
kherel <kherel@users.noreply.github.com>
2017-03-16 17:40:19 +0800

    fix

--- app/assets/javascripts/simpleDropDown.js
+++ app/assets/javascripts/simpleDropDown.js
@@ -5,13 +5,12 @@ function openMobileCatalog() {
 
 window.onclick = function(event) {
   if (!event.target.matches('.mob-header__catalog-btn')) {
-    
     var dropdowns = document.getElementsByClassName("mob-header__catalog-content");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('header__catalog_show')) {
-        openDropdown.classList.remove('header__catalog_show');
+      if (openDropdown.classList.contains('mob-header__catalog_show')) {
+        openDropdown.classList.remove('mob-header__catalog_show');
       }
     }
   }
